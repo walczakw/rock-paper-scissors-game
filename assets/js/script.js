6b9d1d8830126f808ef4527bb54282bb8f36f8d4
walczakw <walczakw864@gmail.com>
2023-03-02 20:13:22 +0000

    Add comments to js file to help with planning

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// 1.
+// User presses a button - need click/touch event
+// (event listeners or a function that takes an event parameter)
+
+// 2.
+// Computer picks a random number between 1 and 3 - Math.random?
+
+// 3.
+// Assign random numer to a button (rock, paper or scissors)
+
+// 4.
+// If the user picks "paper" and computer picks "rock" - user wins - function for winGame?
+//      display message: "You win! You picked paper, computer picked rock"
+//     -> increment user-score
+//
+// else if the user picks "rock" and computer picks "scissors" - user wins - function for winGame?
+//      display message: "You win! You picked rock, computer picked scissors"
+//     -> increment user-score
+//
+// else if the user picks "scissors" and computer picks "paper" - user wins - function for winGame?
+//      display message: "You win! You picked scissors, computer picked paper"
+//     -> increment user-score
+//
+//  else if the user picks "X" and computer picks "X" - it's a draw - function for tieGame?
+//      display message: "It's a draw! You picked X, computer picked X"
+//  
+//  else - computer wins - function for loseGame?
+//      display message: "You lose! You picked ..., computer picked ..."
+//     -> increment computer-score
+
+//5. 
+// If (computer-score or user-score = 5) stop the game and display message "You Won!" or "Computer Won!"
+
+// 6. 
+// Button to restart the game?
